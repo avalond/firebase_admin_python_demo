ea66d0aafd6f0d7b5d47166bc418db42e4d8b85d
Kevin Duan <agonyice0115@gmail.com>
2018-02-15 10:46:03 +0800

    Initial commit

---
+++ userManger.py
@@ -0,0 +1,23 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import auth
+
+cred = credentials.Certificate("./config/firebase-config.json")
+admin = firebase_admin.initialize_app(cred)
+
+# get all of user
+
+page = auth.list_users()
+while page:
+    for user in page.users:
+        print("user-->>" + user.uid)
+        # get next batch of users
+
+        page = page.get_next_page()
+
+#
+# Iterate through all users. This will still retrieve users in batches,
+# buffering no more than 1000 users in memory at a time.
+
+for user in auth.list_users().iterate_all():
+    print("users--------->>" + user.uid)
